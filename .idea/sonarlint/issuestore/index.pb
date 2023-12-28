9c92afac3238053e23a6a3fdb549df7e6972cc44
the9kim <the9rla@gmail.com>
2023-12-28 19:34:18 +0900

    chore: 프로젝트 설정

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
+g
+7src/main/java/web/teambyteam/TeamByTeamApplication.java,0/5/0510c6b07b6a1233205cb305f77ec4b1e266a256
---
+++ src/main/java/web/teambyteam/TeamByTeamApplication.java
@@ -0,0 +1,12 @@
+package web.teambyteam;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamByTeamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeamByTeamApplication.class, args);
+    }
+}
