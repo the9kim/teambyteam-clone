93999955b6599dfa5051643e770c49a759008a2c
the9kim <the9rla@gmail.com>
2024-01-13 09:36:03 +0900

    feat: test api

--- src/main/java/web/teambyteam/TeamByTeamApplication.java
+++ src/main/java/web/teambyteam/TeamByTeamApplication.java
@@ -3,10 +3,26 @@ package web.teambyteam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TeamByTeamApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeamByTeamApplication.class, args);
+    }
+
+}
+
+@RestController
+class Hello {
+    @RequestMapping("/")
+    String index() {
+        return "hello";
+    }
+
+    @RequestMapping("/name")
+    String name() {
+        return "roy";
     }
 }
