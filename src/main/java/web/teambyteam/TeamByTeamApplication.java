45b9fe2d74290eea11a8eb90513da2190fc0d881
the9kim <the9rla@gmail.com>
2024-01-17 12:30:30 +0900

    db-connection-test

--- src/main/java/web/teambyteam/TeamByTeamApplication.java
+++ src/main/java/web/teambyteam/TeamByTeamApplication.java
@@ -8,6 +8,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TeamByTeamApplication {
     public static void main(String[] args) {
         SpringApplication.run(TeamByTeamApplication.class, args);
-
     }
 }
