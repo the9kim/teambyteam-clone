aed9c771811c79ea9e3976efde9d58924a69fb2c
the9kim <the9rla@gmail.com>
2024-01-18 13:42:51 +0900

    feat: Implement Name class for Member and Testing

---
+++ src/main/java/web/teambyteam/member/domain/vo/Name.java
@@ -0,0 +1,34 @@
+package web.teambyteam.member.domain.vo;
+
+import lombok.Getter;
+import web.teambyteam.member.exception.MemberException;
+
+import java.util.Objects;
+
+@Getter
+public class Name {
+
+    public static final int MAX_LENGTH = 20;
+    private final String value;
+
+    public Name(String value) {
+        validateNull(value);
+        String trimmedValue = value.trim();
+        validate(trimmedValue);
+        this.value = trimmedValue;
+    }
+
+    public void validateNull(String value) {
+        if (Objects.isNull(value)) {
+            throw new NullPointerException();
+        }
+    }
+
+    public void validate(String value) {
+        if (value.length() > MAX_LENGTH) {
+            throw new MemberException.NameLengthException(MAX_LENGTH, value);
+        }
+    }
+
+
+}
