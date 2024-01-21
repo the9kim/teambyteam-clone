2be6a58754830eeea9e992b1d460e1e042a218b3
the9kim <the9rla@gmail.com>
2024-01-21 09:39:37 +0900

    feat: Implement the Name class(Vo) of the TeamPlace

---
+++ src/main/java/web/teambyteam/teamplace/domain/exception/TeamPlaceException.java
@@ -0,0 +1,19 @@
+package web.teambyteam.teamplace.domain.exception;
+
+public class TeamPlaceException extends RuntimeException {
+
+    public TeamPlaceException(String message) {
+        super(message);
+    }
+
+    public static class NameLengthException extends TeamPlaceException {
+
+        public NameLengthException(int maxLength, int inputLength) {
+            super(String.format(
+                    "이름이 최대 길이를 초과했습니다. - request info { allowed_length : %d, input_length :%d }"
+                    , maxLength
+                    , inputLength ));
+        }
+    }
+
+}
---
+++ src/test/java/web/teambyteam/teamplace/domain/vo/NameTest.java
@@ -0,0 +1,58 @@
+package web.teambyteam.teamplace.domain.vo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import web.teambyteam.teamplace.domain.exception.TeamPlaceException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class NameTest {
+
+    @Test
+    void createTeamPlaceName() {
+        // given
+        String name = "my team";
+
+        // when
+        assertDoesNotThrow(() -> new Name(name));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"  myteam", "myteam  ", "    myteam    "})
+    void trimName(String teamWithBlank) {
+        // given
+        String expected = "myteam";
+
+        // when
+        Name name = new Name(teamWithBlank);
+
+        // then
+        assertThat(name.getValue()).isEqualTo(expected);
+    }
+
+    @Test
+    void exceptionWithWrongNameLength() {
+        // given
+        String wrongName = "a".repeat(21);
+
+        // when & then
+        assertThatThrownBy(() -> new Name(wrongName))
+                .isInstanceOf(TeamPlaceException.NameLengthException.class)
+                .hasMessage(String.format("이름이 최대 길이를 초과했습니다. - request info { allowed_length : 20, input_length :%d }"
+                        , wrongName.length())
+                );
+    }
+
+    @Test
+    void exceptionWithNullName() {
+        // given
+        String wrongName = null;
+
+        // when & then
+        assertThatThrownBy(() -> new Name(wrongName))
+                .isInstanceOf(NullPointerException.class);
+    }
+}
